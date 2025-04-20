89458b3e44c00879599a36e7699e4c69eae34df4
August Saint Freytag <me@augustfreytag.com>
2025-04-20 13:51:50 +0200

    Introduce util to det. effective item temperature

---
+++ src/main/java/thermite/therm/util/ItemTemperatureUtil.java
@@ -0,0 +1,52 @@
+package thermite.therm.util;
+
+import java.util.HashMap;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import thermite.therm.ThermMod;
+
+public final class ItemTemperatureUtil {
+
+	private static HashMap<String, Double> armorItems = allArmorItemsFromConfig();
+
+	public static double temperatureDeltaForAllArmorItems(ServerPlayerEntity player) {
+		var temperature = 0f;
+
+		for (var itemStack : player.getArmorItems()) {
+			var temperatureValue = temperatureValueForItem(player, itemStack);
+			temperature += temperatureValue;
+		}
+
+		return temperature;
+	}
+
+	public static double temperatureValueForItem(ServerPlayerEntity player, ItemStack itemStack) {
+		var itemId = itemStack.getTranslationKey();
+
+		if (!armorItems.containsKey(itemId)) {
+			return 0.0;
+		}
+
+		var itemTemperature = armorItems.get(itemId);
+		var itemWoolTemperature = woolValueForItem(itemStack);
+
+		return itemTemperature + itemWoolTemperature;
+	}
+
+	private static double woolValueForItem(ItemStack item) {
+		return (double) item.getNbt().getInt("wool");
+	}
+
+	private static HashMap<String, Double> allArmorItemsFromConfig() {
+		var allArmorItems = new HashMap<String, Double>();
+
+		allArmorItems.putAll(ThermMod.config.bootTemperatureItems);
+		allArmorItems.putAll(ThermMod.config.helmetTemperatureItems);
+		allArmorItems.putAll(ThermMod.config.chestplateTemperatureItems);
+		allArmorItems.putAll(ThermMod.config.leggingTemperatureItems);
+
+		return allArmorItems;
+	}
+
+}
