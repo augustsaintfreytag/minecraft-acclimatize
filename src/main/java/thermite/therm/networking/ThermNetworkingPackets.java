943262ead994222325fffddebcd8646fd5fad342
August Saint Freytag <me@augustfreytag.com>
2025-04-20 13:54:46 +0200

    Refactor and clean references in net. packets

--- src/main/java/thermite/therm/networking/ThermNetworkingPackets.java
+++ src/main/java/thermite/therm/networking/ThermNetworkingPackets.java
@@ -6,46 +6,47 @@ import net.minecraft.util.Identifier;
 import thermite.therm.ThermClient;
 import thermite.therm.ThermMod;
 import thermite.therm.networking.packet.DrinkIceJuiceC2SPacket;
-import thermite.therm.networking.packet.PlayerTempTickC2SPacket;
+import thermite.therm.networking.packet.PlayerTemperatureTickC2SPacket;
 
 public class ThermNetworkingPackets {
 
-	// c2s
+	// Client to Server
+
 	public static final Identifier PLAYER_TEMP_TICK_C2S_PACKET_ID = new Identifier(ThermMod.modid,
 			"player_temp_tick_c2s_packet");
-	public static final Identifier DRINK_ICE_JUICE_C2S_PACKET_ID = new Identifier(ThermMod.modid,
-			"drink_ice_juice_c2s_packet");
+	public static final Identifier DRINK_ICE_WATER_C2S_PACKET_ID = new Identifier(ThermMod.modid,
+			"drink_ice_water_c2s_packet");
 
-	// s2c
+	// Server to Client
+
 	public static final Identifier SEND_THERMPLAYERSTATE_S2C_PACKET_ID = new Identifier(ThermMod.modid,
 			"send_thermplayerstate_s2c_packet");
 
 	public static void registerC2SPackets() {
-
-		ServerPlayNetworking.registerGlobalReceiver(PLAYER_TEMP_TICK_C2S_PACKET_ID, PlayerTempTickC2SPacket::receive);
-		ServerPlayNetworking.registerGlobalReceiver(DRINK_ICE_JUICE_C2S_PACKET_ID, DrinkIceJuiceC2SPacket::receive);
+		ServerPlayNetworking.registerGlobalReceiver(PLAYER_TEMP_TICK_C2S_PACKET_ID,
+				PlayerTemperatureTickC2SPacket::receive);
+		ServerPlayNetworking.registerGlobalReceiver(DRINK_ICE_WATER_C2S_PACKET_ID, DrinkIceJuiceC2SPacket::receive);
 
 	}
 
-	public static void registerS2CPackets() {
+	// Registration
 
+	public static void registerS2CPackets() {
 		ClientPlayNetworking.registerGlobalReceiver(SEND_THERMPLAYERSTATE_S2C_PACKET_ID,
 				(client, handler, buf, responseSender) -> {
 					double temperature = buf.readDouble();
-					short td = buf.readShort();
+					short temperatureDifference = buf.readShort();
 					double windPitch = buf.readDouble();
 					double windYaw = buf.readDouble();
 					double windTemp = buf.readDouble();
-					client.execute(() -> {
 
+					client.execute(() -> {
 						ThermClient.clientStoredTemperature = Math.round(temperature);
-						ThermClient.clientStoredTempDir = td;
+						ThermClient.clientStoredTemperatureDifference = temperatureDifference;
 						ThermClient.clientStoredWindPitch = windPitch;
 						ThermClient.clientStoredWindYaw = windYaw;
-						ThermClient.clientStoredWindTemp = windTemp;
-
+						ThermClient.clientStoredWindTemperature = windTemp;
 					});
 				});
-
 	}
 }
