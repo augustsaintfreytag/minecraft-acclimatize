e3a092a52009973c63e29282f693e3950a32cf13
August Saint Freytag <me@augustfreytag.com>
2025-04-26 14:41:20 +0200

    Add config option for acclim. boost factor

--- src/main/java/thermite/therm/ThermConfig.java
+++ src/main/java/thermite/therm/ThermConfig.java
@@ -130,6 +130,10 @@ public class ThermConfig implements ConfigData {
 	public double blockTemperatureDistanceFalloffFactor = 0.075;
 
 	@ConfigEntry.Category("blocks")
+	@Comment("The factor applied to base acclimatization rate when close to a block heat source. (Default: 0.5)")
+	public double blockAcclimatizationBoostFactor = 2.5;
+
+	@ConfigEntry.Category("blocks")
 	@Comment("Blocks that will heat you up when near.")
 	public String heatingBlocks = String.join(", ",
 			"minecraft:fire = 5.0",
