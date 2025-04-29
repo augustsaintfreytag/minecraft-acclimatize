bcd19384b34c2efa8cdc09adbbfbaad247740cc2
August Saint Freytag <me@augustfreytag.com>
2025-04-29 14:58:47 +0200

    Refactor space util, displace individual cast

--- src/main/java/net/saint/acclimatize/util/SpaceUtil.java
+++ src/main/java/net/saint/acclimatize/util/SpaceUtil.java
@@ -10,22 +10,25 @@ import net.saint.acclimatize.Mod;
 public final class SpaceUtil {
 
 	private static final double CONE_ANGLE = Math.toRadians(45);
+	private static final double BASE_COS_ANGLE = MathUtil.approximateCos(CONE_ANGLE);
+	private static final double BASE_SIN_ANGLE = MathUtil.approximateSin(CONE_ANGLE);
+
+	// Checks
 
 	public static boolean checkPlayerIsInInterior(ServerPlayerEntity player) {
 		var profile = Mod.PROFILER.begin("space_check");
 
 		var world = player.getWorld();
-		var origin = player.getPos();
 
 		// Pre-check by raycasting once straight up from player position.
-		if (!preCheckRaycastForPositionInInterior(world, player, origin)) {
+		if (!preCheckRaycastForPositionInInterior(world, player)) {
 			// Ray hit a block, assume indoors.
 			profile.end();
 			Mod.LOGGER.info("Space check raycast (hit sky, pre-check only): " + profile.getDescription());
 			return false;
 		}
 
-		if (!checkRaycastForPositionInInterior(world, player, origin)) {
+		if (!getRaycastResultForPositionInInterior(world, player)) {
 			// Ray sequence hit a block, assume indoors.
 			profile.end();
 			Mod.LOGGER.info(
@@ -39,13 +42,13 @@ public final class SpaceUtil {
 		return true;
 	}
 
-	private static boolean preCheckRaycastForPositionInInterior(World world, ServerPlayerEntity player,
-			Vec3d origin) {
-		final var rayLength = Mod.CONFIG.spaceRayLength;
-		final var direction = new Vec3d(0, 1, 0);
-		final var target = origin.add(direction.multiply(rayLength));
+	private static boolean preCheckRaycastForPositionInInterior(World world, ServerPlayerEntity player) {
+		var origin = player.getPos();
+		var rayLength = Mod.CONFIG.spaceRayLength;
+		var direction = new Vec3d(0, 1, 0);
+		var target = origin.add(direction.multiply(rayLength));
 
-		final var preCheckHitResult = world.raycast(new RaycastContext(
+		var preCheckHitResult = world.raycast(new RaycastContext(
 				origin, target,
 				RaycastContext.ShapeType.COLLIDER,
 				RaycastContext.FluidHandling.NONE,
@@ -59,34 +62,35 @@ public final class SpaceUtil {
 		return true;
 	}
 
-	private static boolean checkRaycastForPositionInInterior(World world, ServerPlayerEntity player,
-			Vec3d origin) {
-		final var baseCosAngle = MathUtil.approximateCos(CONE_ANGLE);
-		final var baseSinAngle = MathUtil.approximateSin(CONE_ANGLE);
-		final var numberOfRays = Mod.CONFIG.spaceNumberOfRays;
-		final var rayLength = Mod.CONFIG.spaceRayLength;
+	private static boolean getRaycastResultForPositionInInterior(World world, ServerPlayerEntity player) {
+		var numberOfRays = Mod.CONFIG.spaceNumberOfRays;
 
 		for (int i = 0; i < numberOfRays; i++) {
-			var theta = 2 * Math.PI * i / numberOfRays;
-			var direction = new Vec3d(
-					baseSinAngle * MathUtil.approximateCos(theta),
-					baseCosAngle,
-					baseSinAngle * MathUtil.approximateSin(theta));
-			var target = origin.add(direction.multiply(rayLength));
-
-			var hitResult = world.raycast(new RaycastContext(
-					origin, target,
-					RaycastContext.ShapeType.COLLIDER,
-					RaycastContext.FluidHandling.NONE,
-					player));
-
-			if (hitResult.getType() == HitResult.Type.MISS) {
-				// Ray hit the sky, assume outdoors.
+			if (performSingleSpaceRaycast(world, player, i)) {
+				// Ray hit a block, assume indoors.
 				return false;
 			}
 		}
 
 		return true;
+	}
+
+	private static boolean performSingleSpaceRaycast(World world, ServerPlayerEntity player, int offset) {
+		var origin = player.getPos();
+		var theta = 2 * Math.PI * offset / Mod.CONFIG.spaceNumberOfRays;
+		var direction = new Vec3d(
+				BASE_SIN_ANGLE * MathUtil.approximateCos(theta),
+				BASE_COS_ANGLE,
+				BASE_SIN_ANGLE * MathUtil.approximateSin(theta));
+		var target = origin.add(direction.multiply(Mod.CONFIG.spaceRayLength));
+
+		var hitResult = world.raycast(new RaycastContext(
+				origin, target,
+				RaycastContext.ShapeType.COLLIDER,
+				RaycastContext.FluidHandling.NONE,
+				player));
+
+		return hitResult.getType() == HitResult.Type.MISS;
 	}
 
 }
