281ebae5126c2f33e0db81550ed49bcdcd831846
August Saint Freytag <me@augustfreytag.com>
2025-05-02 14:51:14 +0200

    Remove unimplemented seasons system from mod

--- src/main/java/net/saint/acclimatize/ModConfig.java
+++ src/main/java/net/saint/acclimatize/ModConfig.java
@@ -314,34 +314,4 @@ public class ModConfig implements ConfigData {
 	@Comment("How many blocks long wind rays are. Increase for larger spaces. (Default: 10)")
 	public int windRayLength = 10;
 
-	// Seasons
-
-	@ConfigEntry.Category("seasons")
-	@Comment("(Experimental) A small built in season system that affects your temperature depending on the season. You can configure the length of each season in half seconds, (one minecraft day = 2400 half seconds).")
-	public boolean enableSeasonSystem = false;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("Length of spring (Default: 48000 half seconds = 20 days).")
-	public long springSeasonLength = 48000;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("Length of summer (Default 48000 half seconds = 20 days).")
-	public long summerSeasonLength = 48000;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("Length of fall (Default 48000 half seconds = 20 days).")
-	public long fallSeasonLength = 48000;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("Length of winter (Default 48000 half seconds = 20 days).")
-	public long winterSeasonLength = 48000;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("Multiplier for how much seasons affect your temperature.")
-	public float seasonTemperatureExtremenessFactor = 1.0f;
-
-	@ConfigEntry.Category("seasons")
-	@Comment("(Experimental) Makes weather reflect the current season. If you enable this make sure to run (/gamerule doWeatherCycle false) to disable the vanilla weather cycle.")
-	public boolean seasonalWeather = false;
-
 }
