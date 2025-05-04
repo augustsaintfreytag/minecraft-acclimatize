162d63601e1f05d6a1210a656de9a5ecadff6956
August Saint Freytag <me@augustfreytag.com>
2025-05-04 12:29:44 +0200

    Remove server state keys for seasons system

--- src/main/java/net/saint/acclimatize/server/ServerState.java
+++ src/main/java/net/saint/acclimatize/server/ServerState.java
@@ -5,22 +5,21 @@ import java.util.UUID;
 
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.PersistentState;
+import net.saint.acclimatize.Mod;
 import net.saint.acclimatize.player.PlayerState;
 
 public class ServerState extends PersistentState {
 
-	// Server metadata
 	public String worldVersion = "4.1.0.8";
+	// World Metadata
 
-	// Seasonal state
-	public int season = 0;
-	public int seasonTick = 0;
-	public long currentSeasonTick = 0;
-	public int seasonalWeatherTick = 0;
 
-	// Wind parameters
+	// Wind
+
 	public double windDirection = 0.0;
 	public double windIntensity = 0.0;
+
+	// Player State
 
 	public HashMap<UUID, PlayerState> players = new HashMap<>();
 
@@ -44,10 +43,6 @@ public class ServerState extends PersistentState {
 		// Server State Properties
 
 		nbt.putString(ServerStateNBTKeys.worldVersion, worldVersion);
-		nbt.putInt(ServerStateNBTKeys.season, season);
-		nbt.putInt(ServerStateNBTKeys.seasonTick, seasonTick);
-		nbt.putLong(ServerStateNBTKeys.currentSeasonTick, currentSeasonTick);
-		nbt.putInt(ServerStateNBTKeys.seasonalWeatherTick, seasonalWeatherTick);
 		nbt.putDouble(ServerStateNBTKeys.windDirection, windDirection);
 		nbt.putDouble(ServerStateNBTKeys.windIntensity, windIntensity);
 
@@ -70,10 +65,6 @@ public class ServerState extends PersistentState {
 		// Server State Properties
 
 		serverState.worldVersion = tag.getString(ServerStateNBTKeys.worldVersion);
-		serverState.season = tag.getInt(ServerStateNBTKeys.season);
-		serverState.seasonTick = tag.getInt(ServerStateNBTKeys.seasonTick);
-		serverState.currentSeasonTick = tag.getLong(ServerStateNBTKeys.currentSeasonTick);
-		serverState.seasonalWeatherTick = tag.getInt(ServerStateNBTKeys.seasonalWeatherTick);
 		serverState.windDirection = tag.getDouble(ServerStateNBTKeys.windDirection);
 		serverState.windIntensity = tag.getDouble(ServerStateNBTKeys.windIntensity);
 
