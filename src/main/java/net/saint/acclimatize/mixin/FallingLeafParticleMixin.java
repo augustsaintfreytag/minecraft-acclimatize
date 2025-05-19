5939c9ad40b2ab24147c1c77c5458451147c6351
August Saint Freytag <me@augustfreytag.com>
2025-05-20 00:50:05 +0200

    Fix annotations on wind coeff. shadow property

--- src/main/java/net/saint/acclimatize/mixin/FallingLeafParticleMixin.java
+++ src/main/java/net/saint/acclimatize/mixin/FallingLeafParticleMixin.java
@@ -1,22 +1,27 @@
 package net.saint.acclimatize.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.SpriteBillboardParticle;
 import net.minecraft.client.world.ClientWorld;
 import randommcsomethin.fallingleaves.particle.FallingLeafParticle;
 
+@Environment(EnvType.CLIENT)
 @Mixin(FallingLeafParticle.class)
 public abstract class FallingLeafParticleMixin extends SpriteBillboardParticle {
 
 	// Properties
 
 	@Shadow
-	protected float windCoefficient = 0;
+	@Final
+	protected final float windCoefficient;
 
 	// Init
 
