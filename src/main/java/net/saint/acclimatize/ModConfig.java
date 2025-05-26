9a782f8a787e0e825402f013cd3d689bc85d235c
August Saint Freytag <me@augustfreytag.com>
2025-05-26 13:59:36 +0200

    Fix comment for altitude factor config option

--- src/main/java/net/saint/acclimatize/ModConfig.java
+++ src/main/java/net/saint/acclimatize/ModConfig.java
@@ -99,7 +99,7 @@ public class ModConfig implements ConfigData {
 	public int altitudeZeroingAnchor = 62;
 
 	@ConfigEntry.Category("world")
-	@Comment("The final applied coefficient for altitude-based temperature deltas. At 1.0, y64 → y0 would be 8 units warmer, y64 → y128 would be 8 units colder. (Default: 1.0)")
+	@Comment("The final applied coefficient for altitude-based temperature deltas. At 1.0, y64 -> y0 would be 8 units warmer, y64 -> y128 would be 8 units colder. (Default: 1.0)")
 	public double altitudeTemperatureFactor = 1.0;
 
 	@ConfigEntry.Category("world")
